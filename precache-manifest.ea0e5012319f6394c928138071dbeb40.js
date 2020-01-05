08741bd9dc3062067054086604035b6b23c81ecd
thnlsn <52093425+thnlsn@users.noreply.github.com>
2020-01-05 02:51:01 -0800

    Updates

---
+++ precache-manifest.ea0e5012319f6394c928138071dbeb40.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b13b45170a8d812be56b41b1c00251",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "3c8a54a0a637eb071413",
+    "url": "/memory-game/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "00c6451337d9c19ae3e9",
+    "url": "/memory-game/static/css/main.9223cf04.chunk.css"
+  },
+  {
+    "revision": "3c8a54a0a637eb071413",
+    "url": "/memory-game/static/js/2.1c95285a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/memory-game/static/js/2.1c95285a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "00c6451337d9c19ae3e9",
+    "url": "/memory-game/static/js/main.4cb2fafd.chunk.js"
+  },
+  {
+    "revision": "ffb99498c6aca822fff7",
+    "url": "/memory-game/static/js/runtime-main.d19c5671.js"
+  },
+  {
+    "revision": "f551cf4455e8a557da135a27a82a5793",
+    "url": "/memory-game/static/media/arya.f551cf44.jpg"
+  },
+  {
+    "revision": "fe115e7b7f9e8198da096f791c3523eb",
+    "url": "/memory-game/static/media/bronn.fe115e7b.jpg"
+  },
+  {
+    "revision": "57bdf533130b9aa6acdd29cc97ba3ed9",
+    "url": "/memory-game/static/media/cersei.57bdf533.jpg"
+  },
+  {
+    "revision": "6017d68003b0038cbcd13b968b4d87a7",
+    "url": "/memory-game/static/media/daenerys.6017d680.jpg"
+  },
+  {
+    "revision": "e6ae3132b77263571e1225b77b142e8c",
+    "url": "/memory-game/static/media/davos.e6ae3132.jpg"
+  },
+  {
+    "revision": "4e4219fe7d0fb87f02cf50e997d7d3f4",
+    "url": "/memory-game/static/media/drogo.4e4219fe.jpg"
+  },
+  {
+    "revision": "064b2fd915f3cbbe4b59aa0fa19da22f",
+    "url": "/memory-game/static/media/jaime.064b2fd9.jpg"
+  },
+  {
+    "revision": "58c2321eb42d7fb50eef26556702a784",
+    "url": "/memory-game/static/media/jon.58c2321e.jpg"
+  },
+  {
+    "revision": "9eea359836b3452c6e01c74aee4e59d2",
+    "url": "/memory-game/static/media/jorah.9eea3598.jpg"
+  },
+  {
+    "revision": "a87f62ec399c9cd91a59c1dcd669329c",
+    "url": "/memory-game/static/media/map.a87f62ec.jpg"
+  },
+  {
+    "revision": "ef7e649a33c8aa1a9f3b45084ca9b85b",
+    "url": "/memory-game/static/media/ned.ef7e649a.jpg"
+  },
+  {
+    "revision": "e7145cc71de03855e58b0e619d0ecff7",
+    "url": "/memory-game/static/media/petyr.e7145cc7.jpg"
+  },
+  {
+    "revision": "93160f0a78923687044fb5e341e40ec0",
+    "url": "/memory-game/static/media/ramsey.93160f0a.jpg"
+  },
+  {
+    "revision": "c332ca94c225b0716f091d4672b9094e",
+    "url": "/memory-game/static/media/sansa.c332ca94.jpg"
+  },
+  {
+    "revision": "8c75608f807e881f6aed72e247666dc0",
+    "url": "/memory-game/static/media/title.8c75608f.png"
+  },
+  {
+    "revision": "057a2f8723d9101b06d375b28f70c89f",
+    "url": "/memory-game/static/media/tormund.057a2f87.jpg"
+  },
+  {
+    "revision": "6dd99ad441aa182e10694774fb5cff8b",
+    "url": "/memory-game/static/media/tyrion.6dd99ad4.jpg"
+  },
+  {
+    "revision": "86ce64b0281681876aa2ac80ac0c4bcb",
+    "url": "/memory-game/static/media/westeros.86ce64b0.jpg"
+  }
+]);
